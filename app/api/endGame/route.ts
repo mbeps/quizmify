0c7f8d8a0d92f6536c4d84b5b5c799ccf273d614
Maruf Bepary <bepary71@gmail.com>
2023-10-23 19:59:01 +0100

    Added documentation for the APIs

--- app/api/endGame/route.ts
+++ app/api/endGame/route.ts
@@ -2,6 +2,16 @@ import { prisma } from "@/lib/db";
 import { endGameSchema } from "@/schemas/questions";
 import { NextResponse } from "next/server";
 
+/**
+ * Finishes the game.
+ * It then validates the request body using the endGameSchema schema.
+ * After validating the request body, the endpoint retrieves the game from the database using the gameId.
+ * If the game is not found, it returns a 404 error response.
+ * If the game is found, the endpoint updates the timeEnded property of the game with the current date and time.
+ * @param req (Request): The request object.
+ * @param res (Response): The response object.
+ * @returns (NextResponse): The response object.
+ */
 export async function POST(req: Request, res: Response) {
   try {
     const body = await req.json();
